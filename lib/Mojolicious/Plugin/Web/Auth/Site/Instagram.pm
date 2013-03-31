07f50f36dbc9bb5fe82acac482b9aa01fc539578
hayajo <hayajo@cpan.org>
2013-03-31 21:05:22 +0900

    Added Mojolicious::Plugin::Web::Auth::Site::Instagram

---
+++ lib/Mojolicious/Plugin/Web/Auth/Site/Instagram.pm
@@ -0,0 +1,15 @@
+package Mojolicious::Plugin::Web::Auth::Site::Instagram;
+
+use Mojo::Base qw/Mojolicious::Plugin::Web::Auth::OAuth2/;
+
+has user_info      => 1;
+has authorize_url    => 'https://api.instagram.com/oauth/authorize';
+has access_token_url => 'https://api.instagram.com/oauth/access_token';
+has user_info_url    => 'https://api.instagram.com/v1/users/self';
+has response_type    => 'code';
+
+has moniker => sub { 'instagram' };
+
+1;
+
+__END__
