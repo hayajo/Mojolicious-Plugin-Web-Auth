42d89fa64277925b131dfef945afe14faea7ea45
hayajo <hayajo@cpan.org>
2013-04-30 23:08:03 +0900

    Removed the dependancy
    - Net::Twitter::Lite
    - WebService::Dropbox

--- lib/Mojolicious/Plugin/Web/Auth/Site/Dropbox.pm
+++ lib/Mojolicious/Plugin/Web/Auth/Site/Dropbox.pm
@@ -1,58 +1,13 @@
 package Mojolicious::Plugin::Web::Auth::Site::Dropbox;
 
-use Mojo::Base -base;
-use Net::OAuth::Client;
-use JSON;
+use Mojo::Base qw/Mojolicious::Plugin::Web::Auth::OAuth/;
 
-has 'key';
-has 'secret';
-has 'user_info' => 1;
+has user_info         => 1;
+has request_token_url => 'https://api.dropbox.com/1/oauth/request_token';
+has authorize_url     => 'https://api.dropbox.com/1/oauth/authorize';
+has access_token_url  => 'https://api.dropbox.com/1/oauth/access_token';
+has user_info_url     => 'https://api.dropbox.com/1/account/info';
 
-sub moniker {'dropbox'}
-
-sub client {
-    my ( $self, $c, $callback_uri ) = @_;
-    $self->{oauth_client} ||= Net::OAuth::Client->new(
-        $self->key,
-        $self->secret,
-        site                => 'https://api.dropbox.com/',
-        request_token_path  => '/1/oauth/request_token',
-        authorize_path      => '/1/oauth/authorize',
-        access_token_path   => '/1/oauth/access_token',
-        access_token_method => 'POST',
-        callback            => $callback_uri,
-        session             => sub { $c->session(@_) },
-    );
-}
-
-sub auth_uri {
-    my ( $self, $c, $callback_uri ) = @_;
-    $callback_uri or die "Missing mandatory parameter: callback_uri";
-    return $self->client( $c, $callback_uri )->authorize_url;
-}
-
-sub callback {
-    my ( $self, $c, $callback ) = @_;
-
-    my $request_token = $c->req->param('oauth_token')
-        or return $callback->{on_error}->("Session error");
-
-    my $access_token = $self->client->get_access_token($request_token);
-
-    my $token  = $access_token->token;
-    my $secret = $access_token->token_secret;
-    my @args   = ( $token, $secret );
-
-    if ( $self->user_info ) {
-        my $res = $access_token->get('/1/account/info');
-        return $callback->{on_error}->( $res->decoded_content )
-            unless ( $res->is_success );
-        push @args, decode_json( $res->decoded_content );
-    }
-
-    return $callback->{on_finished}->(@args);
-}
+has moniker => sub {'dropbox'};
 
 1;
-
-__END__
