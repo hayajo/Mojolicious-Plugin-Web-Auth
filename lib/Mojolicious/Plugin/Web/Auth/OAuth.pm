64619d58ec8f6ec681b62874e855b183097da054
hayajo <hayajo@cpan.org>
2013-05-01 10:24:39 +0900

    Refactored

--- lib/Mojolicious/Plugin/Web/Auth/OAuth.pm
+++ lib/Mojolicious/Plugin/Web/Auth/OAuth.pm
@@ -1,36 +1,16 @@
 package Mojolicious::Plugin::Web::Auth::OAuth;
 
-use Mojo::Base -base;
+use Mojo::Base 'Mojolicious::Plugin::Web::Auth::Base';
 use Net::OAuth::Client;
 use Mojo::JSON;
 
-has 'key';
-has 'secret';
-has 'user_info';
 has 'request_token_url';
-has 'authorize_url';
-has 'access_token_url';
-has 'user_info_url';
-has 'protocol_version' => sub {Net::OAuth::PROTOCOL_VERSION_1_0};
-has moniker            => sub { die 'override me' };
-
-sub _client {
-    my ( $self, $c, $callback_uri ) = @_;
-    $self->{_oauth_client} ||= Net::OAuth::Client->new(
-        $self->key,
-        $self->secret,
-        request_token_path  => $self->request_token_url,
-        authorize_path      => $self->authorize_url,
-        access_token_path   => $self->access_token_url,
-        access_token_method => 'POST',
-        callback            => $callback_uri,
-        session             => sub { $c->session(@_) },
-    );
-}
 
 sub auth_uri {
     my ( $self, $c, $callback_uri ) = @_;
+
     $callback_uri or die "Missing mandatory parameter: callback_uri";
+
     return $self->_client( $c, $callback_uri )->authorize_url;
 }
 
@@ -57,6 +37,18 @@ sub callback {
     return $callback->{on_finished}->(@args);
 }
 
-1;
+sub _client {
+    my ( $self, $c, $callback_uri ) = @_;
+    $self->{_oauth_client} ||= Net::OAuth::Client->new(
+        $self->key,
+        $self->secret,
+        request_token_path  => $self->request_token_url,
+        authorize_path      => $self->authorize_url,
+        access_token_path   => $self->access_token_url,
+        access_token_method => 'POST',
+        callback            => $callback_uri,
+        session             => sub { $c->session(@_) },
+    );
+}
 
-__END__
+1;
