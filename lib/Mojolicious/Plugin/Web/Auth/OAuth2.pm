65ab20a76a7f7c854e10512ca63ccb223e634bb4
hayajo <hayajo@cpan.org>
2014-01-08 12:06:21 +0900

    Refactored

--- lib/Mojolicious/Plugin/Web/Auth/OAuth2.pm
+++ lib/Mojolicious/Plugin/Web/Auth/OAuth2.pm
@@ -37,9 +37,9 @@ sub callback {
         grant_type    => 'authorization_code',
     };
 
-    my $tx = ( $Mojolicious::VERSION < 3.85)
-        ? $self->_ua->post_form( $self->access_token_url => $params ) # Mojo::UserAgent::post_form is deprecated from version 3.85
-        : $self->_ua->post( $self->access_token_url => form => $params );
+    my $tx = ( $Mojolicious::VERSION >= 3.85)
+        ? $self->_ua->post( $self->access_token_url => form => $params )
+        : $self->_ua->post_form( $self->access_token_url => $params ); # Mojo::UserAgent::post_form is deprecated from version 3.85
 
     (my $res = $tx->success ) or do {
         return $callback->{on_error}->( $tx->res->body );
