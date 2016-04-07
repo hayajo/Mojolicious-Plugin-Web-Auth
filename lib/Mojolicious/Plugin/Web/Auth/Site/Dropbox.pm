d450f3e323285ee055749e7d426df3812b2c2cf2
Mikhail Che <mche@users.noreply.github.com>
2016-04-07 16:33:31 +0500

    Update Dropbox.pm

--- lib/Mojolicious/Plugin/Web/Auth/Site/Dropbox.pm
+++ lib/Mojolicious/Plugin/Web/Auth/Site/Dropbox.pm
@@ -10,4 +10,64 @@ has user_info_url     => 'https://api.dropbox.com/1/account/info';
 
 sub moniker {'dropbox'};
 
+has token_from_oauth1_url => 'https://api.dropbox.com/1/oauth2/token_from_oauth1';
+has disable_access_token_url => 'https://api.dropboxapi.com/1/disable_access_token';
+has authorize_header => 'Bearer';
+=pod
+
+#login on OAuth1 and then transitioning from OAuth 1 to OAuth 2 token
+
+$auth->tokenV2fromV1($c, $t, $s);
+$auth->tokenV1disable($c, $t, $s);
+$auth->tokenV2disable($c, $t2);
+
+
+=cut
+sub tokenV2fromV1 {
+    my ($self, $c, $tokenV1, $tokenV1_secret) = @_;
+    my $tx =$c->ua->post($self->token_from_oauth1_url,
+      form=>{
+      "oauth_consumer_key" => $self->key,
+      "oauth_nonce" => $$.$0.time,
+      "oauth_signature" => join('&', $self->secret, $tokenV1_secret),
+      "oauth_signature_method" => "PLAINTEXT",
+      "oauth_timestamp" => time,
+      "oauth_token" => $tokenV1,
+      "oauth_version" => "1.0",
+      },
+    );
+    ( my $res = $tx->success )
+      or $c->app->log->error(join( ' ', 'POST', $self->token_from_oauth1_url, $tx->res->code, $tx->res->default_message ))
+      and return undef;
+    $res->json;
+}
+
+sub tokenV1disable {
+  my ($self, $c, $tokenV1, $tokenV1_secret) = @_;
+  my $tx =$c->ua->post($self->disable_access_token_url,
+      form=>{
+      "oauth_consumer_key" => $self->key,
+      "oauth_nonce" => $$.$0.time,
+      "oauth_signature" => join('&', $self->secret, $tokenV1_secret),
+      "oauth_signature_method" => "PLAINTEXT",
+      "oauth_timestamp" => time,
+      "oauth_token" => $tokenV1,
+      "oauth_version" => "1.0",
+      },
+    );
+    ( my $res = $tx->success )
+      or $c->app->log->error(join( ' ', 'POST', $self->disable_access_token_url, $tx->res->code, $tx->res->default_message ))
+      and return undef;
+    $res->json;#An empty JSON dictionary, which indicates success.
+}
+
+sub tokenV2disable {
+  my ($self, $c, $tokenV2,) = @_;
+  my $tx =$c->ua->post($self->disable_access_token_url, {'Authorization' => $self->authorize_header.' '. $tokenV2});
+  ( my $res = $tx->success )
+    or $c->app->log->error(join( ' ', 'POST', $self->disable_access_token_url, $tx->res->code, $tx->res->default_message ))
+      and return undef;
+  $res->json;#An empty JSON dictionary, which indicates success.
+}
+
 1;
